7e2ddba4b1d0a81ce92186ba133642c10feaf374
michal_nguyen <148235082+MichalNgu@users.noreply.github.com>
2025-07-03 18:02:02 +0200

    Add files via upload

---
+++ first.java
@@ -0,0 +1,16 @@
+public class first {
+    public static void main(String[] args) {
+
+        // String, int, name, double, float, char, boolean
+        String name;
+        int age;
+        name = "michal";
+        age = 16;
+        double num = 4.85;
+        float price = 15.42f;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(num);
+        System.out.println(price);
+    }
+}
