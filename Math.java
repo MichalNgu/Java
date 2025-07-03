29e9562f563c8fee0e5497aa0f82d6eff7633161
michal_nguyen <148235082+MichalNgu@users.noreply.github.com>
2025-07-03 18:11:22 +0200

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,12 @@
+public class Math {
+    public static void main(String[] args) {
+        // math
+        double x = 51.95;
+        double y = 12.74;
+
+        System.out.print(x + y);
+        System.out.print(x - y);
+        System.out.print(x * y);
+        System.out.print(x / y);
+    }
+}
