68acd0a0532c0b1b26e0ebceb2075f561863fb07
michal_nguyen <148235082+MichalNgu@users.noreply.github.com>
2025-07-06 00:02:56 +0200

    Add files via upload

---
+++ switch.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        int day = 2;
+
+        switch(day) {
+            case 1:
+                System.out.println("Monday");
+            case 2:
+                System.out.println("Tuesday");
+            case 3:
+                System.out.println("Wednesday");
+        }
+    }
+}
