1cd2a1eafea7c97288d12a2893c4858395144835
michal_nguyen <148235082+MichalNgu@users.noreply.github.com>
2025-07-07 00:04:52 +0200

    Add files via upload

---
+++ whileL.java
@@ -0,0 +1,12 @@
+class whileL {
+    public static void main(String[] args) {
+        int sum = 0;
+        int num = 0;
+
+        while(num <= 100) {
+            sum += num;
+            num++;
+        }
+        System.out.println(sum);
+        }
+}
