3e365412731349a6a5f3e3c1f480eee1c4554c5c
michal_nguyen <148235082+MichalNgu@users.noreply.github.com>
2025-07-11 03:31:28 +0200

    Add files via upload

---
+++ oop.java
@@ -0,0 +1,12 @@
+class Animal {
+    void bark() {
+        System.out.println("barking...");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal dog = new Animal();
+        dog.bark();
+    }
+}
